72d842da65eeae4f672bc50f2fe1ba358efb56d5
JuditTardis <58729246+JuditTardis@users.noreply.github.com>
2020-02-19 09:24:08 +0100

    Add files via upload

---
+++ ejercicio variables/C#/Exercici 1 fase 1/exercici 1 fase 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercici_1_fase_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "Judit";
+            string cognom1 = "Arias";
+            string cognom2 = "Piferrer";
+            int dia = 15;
+            int mes = 01;
+            int any = 2020;
+            Console.WriteLine(cognom1 + " " + cognom2 + "," + nom);
+            Console.WriteLine(dia + "/" + mes + "/" + any);
+        }
+    }
+}
---
+++ ejercicio variables/C#/exercici 1 fase 2/exercici 1 fase 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercici_1_fase_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int year = 1948;
+            int traspaso = 4;
+            Console.WriteLine("Introduzce tu año de nacimiento");
+            var nacimiento = Convert.ToInt32(Console.ReadLine());
+            int resultado = ((nacimiento - year) / traspaso);
+            Console.WriteLine("Desde el año " + year + " hasta el año " + nacimiento + " ha habido " + resultado + " años de traspaso.");
+        }
+    }
+}
